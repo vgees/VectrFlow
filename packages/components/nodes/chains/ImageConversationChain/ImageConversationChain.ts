752fb13c5b5839e1343961c18e87bcac8fa3b067
Ganapathy Shankar <38199344+vgees@users.noreply.github.com>
2023-10-23 08:39:29 +0530

    Syntax fix in ImageConversationChain.ts

--- packages/components/nodes/chains/ImageConversationChain/ImageConversationChain.ts
+++ packages/components/nodes/chains/ImageConversationChain/ImageConversationChain.ts
@@ -1,35 +1,35 @@
-import { ICommonObject, INode, INodeData, INodeParams } from '../../../src/Interface';
-import { RetrievalQAChain } from 'langchain/chains';
-import { getBaseClasses } from '../../../src/utils';
-import { ConsoleCallbackHandler, additionalCallbacks } from '../../../src/handler';
+import { ICommonObject, INode, INodeData, INodeParams } from '../../../src/Interface'
+import { RetrievalQAChain } from 'langchain/chains'
+import { getBaseClasses } from '../../../src/utils'
+import { ConsoleCallbackHandler, additionalCallbacks } from '../../../src/handler'
 
 class ImageConversationChain_Chains implements INode {
-    label: string;
-    name: string;
-    version: number;
-    type: string;
-    icon: string;
-    category: string;
-    baseClasses: string[];
-    description: string;
-    inputs: INodeParams[];
+    label: string
+    name: string
+    version: number
+    type: string
+    icon: string
+    category: string
+    baseClasses: string[]
+    description: string
+    inputs: INodeParams[]
 
     constructor() {
-        this.label = 'Image Conversation Chain';
-        this.name = 'imageConversationChain';
-        this.version = 1.0;
-        this.type = 'ImageConversationChain';
-        this.icon = 'image.png';
-        this.category = 'Chains';
-        this.description = 'QA chain to answer a question based on the retrieved images';
-        this.baseClasses = [this.type, ...getBaseClasses(RetrievalQAChain)];
+        this.label = 'Image Conversation Chain'
+        this.name = 'imageConversationChain'
+        this.version = 1.0
+        this.type = 'ImageConversationChain'
+        this.icon = 'image.png'
+        this.category = 'Chains'
+        this.description = 'QA chain to answer a question based on the retrieved images'
+        this.baseClasses = [this.type, ...getBaseClasses(RetrievalQAChain)]
         this.inputs = [
             {
                 label: 'Image Description',
                 name: 'imageDescription',
                 type: 'description'
             }
-        ];
+        ]
     }
 
     async init(nodeData: INodeData): Promise<any> {
@@ -38,15 +38,15 @@ class ImageConversationChain_Chains implements INode {
     }
 
     async run(nodeData: INodeData, input: string, options: ICommonObject): Promise<string> {
-        const imageDescription = nodeData.inputs?.imageDescription as string;
-        const loggerHandler = new ConsoleCallbackHandler(options.logger);
-        const callbacks = await additionalCallbacks(nodeData, options);
+        const imageDescription = nodeData.inputs?.imageDescription as string
+        const loggerHandler = new ConsoleCallbackHandler(options.logger)
+        const callbacks = await additionalCallbacks(nodeData, options)
 
         // TODO: Any logic or operations you want to do with the image description can go here
         // For now, it simply returns the description
 
-        return imageDescription;
+        return imageDescription
     }
 }
 
-module.exports = { nodeClass: ImageConversationChain_Chains };
+module.exports = { nodeClass: ImageConversationChain_Chains }
